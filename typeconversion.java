ebbab1d5adbc3c8065566fc956a0325c60d17557
sirisha-s19 <sirishasgowda2007@gmail.com>
2026-09-03 20:37:32 +0530

    Add files via upload

---
+++ typeconversion.java
@@ -0,0 +1,10 @@
+ public class typeconversion {
+    public static void main(String[] args) {
+
+        int num = 100;
+        double value = num;   // int to double
+
+        System.out.println("Integer value: " + num);
+        System.out.println("Double value: " + value);
+    }
+}
