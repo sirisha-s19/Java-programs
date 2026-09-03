0936ece701ad28fa45ff6a2bd2025bc807890604
sirisha-s19 <sirishasgowda2007@gmail.com>
2026-09-03 20:34:37 +0530

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+public class arithmetic {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int result = a + b;
+
+        System.out.println("Addition: " + result);
+    }
+}
