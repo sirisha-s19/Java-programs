7474c2016cada8f6e014d20f144371d72f3817c5
sirisha-s19 <sirishasgowda2007@gmail.com>
2026-09-03 20:36:07 +0530

    Add files via upload

---
+++ ifelse.java
@@ -0,0 +1,11 @@
+public class ifelse {
+    public static void main(String[] args) {
+        int age = 16;
+
+        if (age >= 18) {
+            System.out.println("Eligible to vote");
+        } else {
+            System.out.println("Not eligible to vote");
+        }
+    }
+}
