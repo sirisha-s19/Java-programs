ef442a0c0b336ec8538113fe4bae8c9b6c6eb68c
sirisha-s19 <sirishasgowda2007@gmail.com>
2026-09-03 20:35:13 +0530

    Add files via upload

---
+++ casting.java
@@ -0,0 +1,11 @@
+public class casting {
+    public static void main(String[] args) {
+
+        double a = 10.5;
+
+        int b = (int) a;
+
+        System.out.println("Double value: " + a);
+        System.out.println("Integer value: " + b);
+    }
+}
