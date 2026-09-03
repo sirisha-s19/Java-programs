12d33a95989660dc6e35e7147fb9e165948159e4
sirisha-s19 <sirishasgowda2007@gmail.com>
2026-09-03 20:35:52 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,7 @@
+class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 20; i <= 36; i += 4) {
+            System.out.println("The number is " + i);
+        }
+    }
+}
